5c3a306c71734c5852e046d9fafa14b3a56470af
Maksym <maks.meleschuk@gmail.com>
2023-05-31 00:54:14 +0300

    Initial commit

---
+++ src/main/java/com/epam/esm/SpringbootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestAppApplication.class, args);
+	}
+
+}
